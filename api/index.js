eb8f43800ec320b7d4c2396dc3b124835690021d
Bezhan <beerubk74@gmail.com>
2024-01-11 20:42:55 +0500

    create a test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from './routes/user.route.js'
 dotenv.config();
 
 mongoose
@@ -14,6 +15,8 @@ mongoose
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("serv 3000");
 });
+
+app.use('/api/user', userRoutes)
