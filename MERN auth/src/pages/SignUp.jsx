01fa000bc6e9ea27312ea0970450c655390aa40a
Bezhan <beerubk74@gmail.com>
2024-01-11 23:32:46 +0500

    complete sign up page functionality

--- MERN auth/src/pages/SignUp.jsx
+++ MERN auth/src/pages/SignUp.jsx
@@ -1,31 +1,70 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setError(false);
+      setLoading(true);
+      console.log(formData);
+      console.log(JSON.stringify(formData));
+
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setError(true);
+    }
+  };
+
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign up</h1>
 
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Username"
           id="username"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 p-3 rounded-lg outline-none"
+          onChange={handleChange}
         />
         <input
           type="email"
           placeholder="Example@gmail.com"
           id="email"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 p-3 rounded-lg outline-none"
+          onChange={handleChange}
         />
         <input
           type="password"
           placeholder="Password"
           id="password"
-          className="bg-slate-100 p-3 rounded-lg"
+          className="bg-slate-100 p-3 rounded-lg outline-none"
+          onChange={handleChange}
         />
-        <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70">
-          Sign up
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-70"
+        >
+          {loading ? "Loading.." : "Sign Up"}
         </button>
       </form>
       <div className="flex gap-2 mt-5 font-semibold">
@@ -34,6 +73,11 @@ export default function SignUp() {
           <span className="text-blue-500 hover:underline ">Sign in</span>
         </Link>
       </div>
+      {error && (
+        <p className="text-red-700 font-bold mt-5">
+          Something went wrong :c
+        </p>
+      )}
     </div>
   );
 }
--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ const app = express();
 
 app.use(express.json())
 
-app.listen(3001, () => {
+app.listen(3000, () => {
   console.log("serv 3000");
 });
 
