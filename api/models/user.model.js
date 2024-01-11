39984280dca1755c33a9093a5a14d527812d7676
Bezhan <beerubk74@gmail.com>
2024-01-11 20:30:50 +0500

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        require: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        require: true,
+    }
+
+}, {timestamps: true})
